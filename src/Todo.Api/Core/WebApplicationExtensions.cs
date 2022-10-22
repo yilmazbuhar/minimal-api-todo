268ef8a2ee699829cb75683213d1825bdc0fddef
YILMAZCAN\yilmaz <yilmazbuhar@gmail.com>
2022-10-22 22:53:46 +0300

    fix(validate) common validatation

---
+++ src/Todo.Api/Core/WebApplicationExtensions.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+
+namespace Todo.Api
+{
+    public static class WebApplicationExtensions
+    {
+        public static IEndpointConventionBuilder MapGet<TRequest>(
+            this WebApplication app,
+            string pattern
+            )
+            where TRequest : IBaseRequest, new()
+        {
+            return app.MapGet(pattern, RequestHandler<TRequest>);
+        }
+
+        private static async Task RequestHandler<TRequest>(HttpContext httpContext)
+            where TRequest : IBaseRequest, new()
+        {
+            var _mediator = httpContext.RequestServices.GetRequiredService<IMediator>();
+            var request = await httpContext.ModelBindAsync<TRequest>();
+
+            await _mediator.Send(request);
+        }
+
+        private static async Task<TRequest> ModelBindAsync<TRequest>(this HttpContext ctx)
+            where TRequest : IBaseRequest, new()
+        {
+            var requestType = typeof(TRequest);
+            var interfaces = requestType.GetInterfaces();
+
+            TRequest result = interfaces.Any(x => x.Equals(typeof(IFromJsonBody)))
+                ? (TRequest)await ctx.Request.ReadFromJsonAsync(requestType)
+                : new TRequest();
+
+           if(result is IFromRoute fromRoute)
+                fromRoute.BindFromRoute(ctx.Request.RouteValues);
+
+            return result;
+        }
+    }
+}
