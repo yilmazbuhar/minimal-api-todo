d168b6e54963151402d32fd43a95a78666b43e0a
YILMAZCAN\yilmaz <yilmazbuhar@gmail.com>
2022-10-22 22:55:07 +0300

    feat(validator) validator extension

--- src/Todo.Api/Core/StartupExtensions.cs
+++ src/Todo.Api/Core/StartupExtensions.cs
@@ -1,19 +1,50 @@
 ﻿using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Todo.App;
+using FluentValidation.Results;
+using System.ComponentModel.DataAnnotations;
 
 namespace Todo.Api
 {
     public static class StartupExtensions
     {
-        public static IServiceCollection AddServices(this IServiceCollection services)
+        /// <summary>
+        /// Validate object that given type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="validator"></param>
+        /// <param name="model"></param>
+        /// <param name="endpointFunc"></param>
+        /// <returns></returns>
+        public static async Task<IResult> Validate<T>(this IValidator<T> validator, T model, Func<Task<IResult>> endpointFunc)
+        {
+            var validationResult = await validator.ValidateAsync(model);
+            if (!validationResult.IsValid)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            return await endpointFunc();
+        }
+
+        /// <summary>
+        /// Adds startup services
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<TodoDbContext>(opt =>
             {
-                opt.UseNpgsql("Host=localhost;Database=todoitems;Username=user;Password=5EPZK8rk9$mLJ)d@");
+                opt.UseSqlServer(configuration.GetConnectionString("SqlServer"));
             });
+            services.AddMediatR(typeof(Program));
             services.AddAutoMapper(typeof(Program));
             services.AddValidatorsFromAssemblyContaining<Program>();
-            services.AddScoped<IValidator<TodoItem>, TodoItemValidator>();
+            //services.AddScoped<IValidator<TodoItemSaveModel>, TodoItemValidator>();
+            services.AddValidatorsFromAssemblyContaining<Program>();
 
             // Add services to the container.
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,7 +54,12 @@ namespace Todo.Api
             return services;
         }
 
-        public static WebApplication AddSwagger(this WebApplication app)
+        /// <summary>
+        /// Use swagger UI
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static WebApplication UseSwaggerEndPoints(this WebApplication app)
         {
             if (app.Environment.IsDevelopment())
             {
@@ -34,65 +70,47 @@ namespace Todo.Api
             return app;
         }
 
+        /// <summary>
+        /// Adds a RouteEndpoints to the IEndpointRouteBuilder
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
         public static WebApplication RegisterEndPoints(this WebApplication app)
         {
-            app.MapPost("/todoitem", async (Validator<TodoItem> validator, TodoItem todo, TodoDbContext db) =>
+            app.MapPost("/todoitem", async (IValidator<TodoItemSaveModel> validator, TodoItemSaveModel todo, IMediator mediator) =>
             {
-                var (isValid, value, errors) = validator;
-                if (!isValid)
-                    return Results.BadRequest(errors);
-
-                db.TodoItem.FirstOrDefault();
-                await db.SaveChangesAsync();
-
-                return Results.Created($"/todoitem/{todo.Id}", todo);
-            });
-
-            app.MapPut("/todoitem", async (TodoItem todo, TodoDbContext db) =>
-            {
-                var todoItem = await db.TodoItem.SingleOrDefaultAsync(ti => ti.Id == todo.Id);
-                if (todoItem == null)
-                    return Results.UnprocessableEntity($"todo item with id {todo.Id} not found");
-
-                todoItem.Title = todo.Title;
-                todoItem.DueDate = todo.DueDate;
-
-                await db.SaveChangesAsync();
-
-                return Results.Created($"/todoitem/{todo.Id}", todo);
-            });
-
-            app.MapPut("/todoitem/done/{id}", async (Guid id, TodoDbContext db) =>
-            {
-                var todoItem = await db.TodoItem.FindAsync(id);
-                if (todoItem == null)
-                    return Results.UnprocessableEntity($"todo item with id {id} not found");
-
-                todoItem.Done = true;
-
-                await db.SaveChangesAsync();
-
-                return Results.Ok();
-            });
-
-            app.MapGet("/todoitem", async (TodoDbContext db) =>
-                await db.TodoItem.Where(ti => !ti.Done).ToListAsync());
-
-
-            app.MapGet("/todoitem/overdue", async (TodoDbContext db) =>
-                await db.TodoItem.Where(ti => ti.DueDate < DateTime.Now && !ti.Done).ToListAsync());
-
-            app.MapDelete("/todoitem/{id}", async (Guid id, TodoDbContext db) =>
-            {
-                if (await db.TodoItem.FindAsync(id) is TodoItem todo)
+                await validator.Validate<TodoItemSaveModel>(todo, async () =>
                 {
-                    db.TodoItem.Remove(todo);
-                    await db.SaveChangesAsync();
+                    await mediator.Send(new SaveTodoItemCommand(todo));
 
-                    return Results.Ok(todo);
-                }
+                    return Results.Created($"/todoitem", null);
+                });
+            });
 
-                return Results.NotFound();
+            app.MapPut("/todoitem/{id}", async (IValidator<TodoItemUpdateModel> validator, Guid id, TodoItemUpdateModel todo, IMediator mediator) =>
+            {
+                await validator.Validate<TodoItemUpdateModel>(todo, async () =>
+                {
+                    return await mediator.Send(new UpdateTodoItemCommand(id, todo)) ?
+                        Results.Ok() : Results.UnprocessableEntity($"todo item with id {id} not found");
+
+                });
+            });
+
+            app.MapPut("/todoitem/done/{id}", async (Guid id, IMediator mediator) =>
+            {
+                return await mediator.Send(new SetDoneTodoItemCommand() { Id = id }) ?
+                    Results.Ok() : Results.UnprocessableEntity($"todo item with id {id} not found");
+            });
+
+            app.MapGet("/todoitem/{overdue}", async (bool overdue, IMediator mediator) =>
+            {
+                return await mediator.Send(new GetTodoItemsRequest(overdue));
+            });
+
+            app.MapDelete("/todoitem/{id}", async (Guid id, IMediator mediator) =>
+            {
+                await mediator.Send(new DeleteTodoItemCommand() { Id = id });
             });
 
             return app;
--- src/Todo.Api/Services/Todo/GetTodoItems.cs
+++ src/Todo.Api/Services/Todo/GetTodoItems.cs
@@ -1,24 +1,37 @@
 ﻿using MediatR;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
-namespace minimal_web_api.Services.Todo
+namespace Todo.App
 {
     public class GetTodoItemsRequest : IRequest<List<TodoItem>>
     {
         public bool OnlyOverdue { get; set; }
+
+        public GetTodoItemsRequest(bool onlyOverdue)
+        {
+            OnlyOverdue = onlyOverdue;
+        }
     }
 
-    public class GetTodoItemsRequestHandler : IRequestHandler<GetTodoItemsRequest, List<TodoItem>>
+    public class GetTodoItemByIdRequest : IRequest<TodoItem>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class GetTodoItemsRequestHandler : 
+        IRequestHandler<GetTodoItemsRequest, List<TodoItem>>,
+        IRequestHandler<GetTodoItemByIdRequest, TodoItem>
     {
         private readonly TodoDbContext _todoDbContext;
         public GetTodoItemsRequestHandler(TodoDbContext todoDbContext)
         {
-            _todoDbContext = todoDbContext; 
+            _todoDbContext = todoDbContext;
         }
 
-        public async Task<List<TodoItem>> Handle(GetTodoItemsRequest request, CancellationToken cancellationToken)
-        {
-            return await _todoDbContext.TodoItem.Where(ti => !ti.Done && (request.OnlyOverdue && ti.DueDate < DateTime.Now)).ToListAsync();
-        }
+        public async Task<List<TodoItem>> Handle(GetTodoItemsRequest request, CancellationToken cancellationToken) =>
+            await _todoDbContext.TodoItem.Where(ti => !ti.Done && (request.OnlyOverdue && ti.DueDate<DateTime.Now)).ToListAsync();
+
+        public async Task<TodoItem> Handle(GetTodoItemByIdRequest request, CancellationToken cancellationToken) =>
+            await _todoDbContext.TodoItem.FindAsync(request.Id);
     }
 }
